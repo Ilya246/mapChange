8927206e9071b93b9a3067fb2fdaef413594aa1c
darkness <inflexibled@gmail.com>
2021-10-26 21:11:51 +0300

    Fixes & cleanup & optimisation

--- src/mapChange/mapChange.java
+++ src/mapChange/mapChange.java
@@ -1,20 +1,25 @@
 package mapChange;
 
-import arc.Core.*;
+import arc.Core;
 import arc.Events;
 import arc.struct.ObjectMap;
 import arc.struct.Seq;
-import arc.util.*;
-import mindustry.game.EventType.*;
+import arc.util.CommandHandler;
+import arc.util.Reflect;
+import arc.util.Strings;
+import arc.util.Timer;
+import mindustry.game.EventType.GameOverEvent;
 import mindustry.game.Team;
-import mindustry.gen.*;
+import mindustry.gen.Call;
+import mindustry.gen.Groups;
+import mindustry.gen.Player;
 import mindustry.maps.Map;
 import mindustry.mod.Plugin;
 import mindustry.server.ServerControl;
 
-import static mindustry.Vars.*;
+import static mindustry.Vars.maps;
 
-public class mapChange extends Plugin{
+public final class mapChange extends Plugin{
     public Map currentlyVoting = null;
     public int currectSelectVotes = 0;
     public float selectVoteFraction = 0.6f;
@@ -31,39 +36,39 @@ public class mapChange extends Plugin{
         handler.<Player>register("voteselect", "[name...]", "Vote to select the next map by name or #id. Use without args to display maplist.", (args, player) -> {
             try{
                 if(currentlyVoting != null){
-                    if(args[0].equalsIgnoreCase("yes")){
+                    if(args[0].equalsIgnoreCase("yes") || args[0].equalsIgnoreCase("y")){
                         if(selectVoted.contains(player.uuid())){
                             player.sendMessage("[scarlet]You can't vote twice.");
                         }else{
                             voteSelect(player);
-                        };
+                        }
                     }else{
                         player.sendMessage("[scarlet]A vote is already in progress.");
-                    };
+                    }
                     return;
                 }
                 if(args.length == 0){
                     StringBuilder builder = new StringBuilder();
                     builder.append("[orange]Maps: \n");
                     int id = 0;
-                    for(Map m : maps.all()){
-                        builder.append("[scarlet]id:" + id + "[white] " + m.name() + " ");
+                    for(Map m : maps.customMaps()){
+                        builder.append("[accent]id:").append(id).append("[white] ").append(m.name()).append(" ");
                         id++;
-                    };
+                    }
                     player.sendMessage(builder.toString());
                     return;
-                };
+                }
                 Map found;
                 if(args[0].startsWith("#") && Strings.canParseInt(args[0].substring(1))){
                     int id = Strings.parseInt(args[0].substring(1));
-                    found = maps.all().get(id);
+                    found = maps.customMaps().get(id);
                 }else{
                     found = findMap(args[0]);
-                };
+                }
                 if(found == null){
                     player.sendMessage("[scarlet]No map [orange]'" + args[0] + "'[scarlet] found.");
                     return;
-                };
+                }
                 currentlyVoting = found;
                 voteSelect(player);
                 selectVoteTask = Timer.schedule(() -> {
@@ -73,17 +78,17 @@ public class mapChange extends Plugin{
                     currectSelectVotes = 0;
                 }, selectVoteLength);
             }catch(Exception badArguments){
-                player.sendMessage("Something went wrong.");
-            };
+                player.sendMessage("[scarlet]Something went wrong. Please check command arguments.");
+            }
         });
         handler.<Player>register("voteskip", "<y/n>","Vote to skip the current map.", (args, player) -> {
-            if(args[0].equalsIgnoreCase("y")){
+            if(args[0].equalsIgnoreCase("y") || args[0].equalsIgnoreCase("yes")){
                 voteSkip(player, true);
-            }else if(args[0].equalsIgnoreCase("n")){
+            }else if(args[0].equalsIgnoreCase("n") || args[0].equalsIgnoreCase("no")){
                 voteSkip(player, false);
             }else{
-                player.sendMessage("[scarlet]Voteskip only accepts 'y' or 'n' as arguments.");
-            };
+                player.sendMessage("[scarlet]Voteskip only accepts 'y' (yes) or 'n' (no) as arguments.");
+            }
         });
     }
 
@@ -91,20 +96,20 @@ public class mapChange extends Plugin{
         selectVoted.add(player.uuid());
         currectSelectVotes++;
         int voteCap = (int)Math.ceil((float)Groups.player.size() * selectVoteFraction);
-        Call.sendMessage(player.name + " [lightgray]has voted to select " + currentlyVoting.name() + " [lightgray]as the next map (" + currectSelectVotes + "/" + voteCap + "). Type '/voteselect yes' to agree.");
+        Call.sendMessage(player.coloredName() + " [lightgray]has voted to select " + currentlyVoting.name() + " [lightgray]as the next map (" + currectSelectVotes + "/" + voteCap + "). Type '/voteselect yes' to agree.");
         if(currectSelectVotes >= voteCap){
-            arc.Core.app.getListeners().each(lst -> {
+            Core.app.getListeners().each(lst -> {
                 if(lst instanceof ServerControl){
                     ServerControl scont = (ServerControl)lst;
-                    scont.handler.handleMessage("nextmap " + currentlyVoting.name());
-                };
+                    Reflect.set(scont, "nextMapOverride", currentlyVoting);
+                }
             });
             Call.sendMessage("[accent]Next map overridden to be " + currentlyVoting.name() + "[accent].");
             currentlyVoting = null;
             selectVoted.clear();
             currectSelectVotes = 0;
             selectVoteTask.cancel();
-        };
+        }
     }
     public void voteSkip(Player player, boolean agree){
         int voteCap = (int)Math.ceil((float)Groups.player.size() * skipVoteFraction);
@@ -115,11 +120,11 @@ public class mapChange extends Plugin{
                 }else{
                     Call.sendMessage("[lightgray]Map skip vote successful.");
                     Events.fire(new GameOverEvent(Team.derelict));
-                };
+                }
                 skipVoted.clear();
                 currentSkipVotes = 0;
             }, skipVoteLength);
-        };
+        }
         if(skipVoted.containsKey(player.uuid())){
             if(skipVoted.get(player.uuid()) != agree){
                 currentSkipVotes += agree ? 1 : -1;
@@ -129,13 +134,13 @@ public class mapChange extends Plugin{
                 player.sendMessage("[scarlet]You can't vote twice.");
                 return;
             }
-        };
+        }
         skipVoted.put(player.uuid(), agree);
         currentSkipVotes += agree ? 1 : -1;
-        Call.sendMessage(player.name + " [lightgray]has voted to skip map (" + currentSkipVotes + "/" + voteCap + "). Type '/voteskip y/n' to agree or disagree.");
+        Call.sendMessage(player.coloredName() + " [lightgray]has voted to skip map (" + currentSkipVotes + "/" + voteCap + "). Type '/voteskip y/n' to agree or disagree.");
     }
 
     public Map findMap(String mapName){
-        return maps.all().find(map -> Strings.stripColors(map.name().replace('_', ' ')).equalsIgnoreCase(Strings.stripColors(mapName).replace('_', ' ')));
+        return maps.customMaps().find(map -> Strings.stripColors(map.name().replace('_', ' ')).equalsIgnoreCase(Strings.stripColors(mapName).replace('_', ' ')));
     }
 }
